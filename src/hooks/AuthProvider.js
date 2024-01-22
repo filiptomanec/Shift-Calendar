44ae34b55967301b752e8bc77c66251535589529
Filip Tomanec <filip.tomanec@artio.cz>
2024-01-22 12:06:47 +0100

    Added router and authentication

---
+++ src/hooks/AuthProvider.js
@@ -0,0 +1,53 @@
+import { useContext, createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem("site") || "");
+    const navigate = useNavigate();
+    const loginAction = async (data) => {
+        try {
+            const response = await fetch("http://localhost:3004/auth/authenticate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            const res = await response.json();
+            console.log(res);
+            if (res) {
+                setUser(res.username);
+                setToken(res.accessToken);
+                localStorage.setItem("site", res.accessToken);
+                navigate("/dashboard");
+                return;
+            }
+            throw new Error(res.message);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const logOut = () => {
+        setUser(null);
+        setToken("");
+        localStorage.removeItem("site");
+        navigate("/");
+    };
+
+    return (
+        <AuthContext.Provider value={{ token, user, loginAction, logOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
